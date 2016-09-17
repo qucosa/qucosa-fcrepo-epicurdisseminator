03fc715389cbbc487518a0060290d0d64e8b744b
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2016-09-17 09:21:56 +0200

    Dummy Epicur output

--- src/main/java/de/qucosa/dissemination/epicur/servlet/EpicurDisseminationServlet.java
+++ src/main/java/de/qucosa/dissemination/epicur/servlet/EpicurDisseminationServlet.java
@@ -16,7 +16,65 @@
 
 package de.qucosa.dissemination.epicur.servlet;
 
+import de.dnb.xepicur.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
 
 public class EpicurDisseminationServlet extends HttpServlet {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private JAXBContext jaxbContext;
+    private Marshaller marshaller;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            jaxbContext = JAXBContext.newInstance(Epicur.class);
+            marshaller = jaxbContext.createMarshaller();
+        } catch (JAXBException e) {
+            throw new ServletException("Failed to initialize servlet", e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            Epicur epicur = new Epicur();
+
+            UpdateStatusType updateStatus = new UpdateStatusType();
+            updateStatus.setType("urn_new");
+
+            DeliveryType delivery = new DeliveryType();
+            delivery.setUpdateStatus(updateStatus);
+
+            AdministrativeDataType administrativeData = new AdministrativeDataType();
+            administrativeData.setDelivery(delivery);
+
+            epicur.setAdministrativeData(administrativeData);
+
+            IdentifierType identifier = new IdentifierType();
+            identifier.setScheme("urn:nbn:de");
+            identifier.setValue("urn:nbn:de:bsz:14-qucosa-201469");
+
+            RecordType record = new RecordType();
+            record.setIdentifier(identifier);
+
+            epicur.getRecord().add(record);
+
+            marshaller.marshal(epicur, resp.getWriter());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            resp.sendError(500);
+        }
+    }
+
 }
