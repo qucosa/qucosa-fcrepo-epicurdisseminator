d1dd27564fd7a7f20c39b7bf82e60304e3159aa3
dseelig <daniel.seelig@slub-dresden.de>
2018-06-26 10:39:20 +0200

    Remove print out in test method. Remname test method. Add private method for serialize epicur document.

--- src/test/java/de/qucosa/dissemination/epicur/mapper/EpicurMapperTest.java
+++ src/test/java/de/qucosa/dissemination/epicur/mapper/EpicurMapperTest.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
 import org.jdom2.JDOMException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -21,20 +22,22 @@ import de.qucosa.dissemination.epicur.EpicurDissMapper;
 public class EpicurMapperTest {
     
     @Test
-    public void mapper_Test() throws ParserConfigurationException, SAXException, IOException, JDOMException, JAXBException, EpicurBuilderException {
+    public void Find_epicur_tag_in_mapped_document() throws ParserConfigurationException, SAXException, IOException, JDOMException, JAXBException, EpicurBuilderException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
-        Document mets = db.parse(getClass().getClassLoader().getResource("hochschulschrift_TEST.xml").getPath());
-        
+        Document mets = db.parse(getClass().getResourceAsStream("/hochschulschrift_TEST.xml"));
         EpicurDissMapper mapper = new EpicurDissMapper("http://test.##AGENT##.qucosa.de/id/##PID##", "", "", true);
-        
         Document epicurRes = mapper.transformEpicurDiss(mets);
         
-        OutputFormat outputFormat = new OutputFormat(epicurRes);
+        Assert.assertEquals("epicur", epicurRes.getDocumentElement().getTagName());
+    }
+
+    private XMLSerializer serializeEpicur(Document document) {
+        OutputFormat outputFormat = new OutputFormat(document);
         outputFormat.setOmitXMLDeclaration(true);
         StringWriter stringWriter = new StringWriter();
         XMLSerializer serialize = new XMLSerializer(stringWriter, outputFormat);
-        serialize.serialize(epicurRes);
+        return serialize;
     }
 }
