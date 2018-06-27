ca9e6556751aa9715f2982944ced4503fdc088d8
dseelig <daniel.seelig@slub-dresden.de>
2018-06-27 07:57:29 +0200

    Remove unused method.

--- src/test/java/de/qucosa/dissemination/epicur/mapper/EpicurMapperTest.java
+++ src/test/java/de/qucosa/dissemination/epicur/mapper/EpicurMapperTest.java
@@ -1,23 +1,18 @@
 package de.qucosa.dissemination.epicur.mapper;
 
-import java.io.IOException;
-import java.io.StringWriter;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.XMLSerializer;
+import de.qucosa.dissemination.epicur.EpicurBuilderException;
+import de.qucosa.dissemination.epicur.EpicurDissMapper;
 import org.jdom2.JDOMException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import de.qucosa.dissemination.epicur.EpicurBuilderException;
-import de.qucosa.dissemination.epicur.EpicurDissMapper;
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class EpicurMapperTest {
     
@@ -26,18 +21,10 @@ public class EpicurMapperTest {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
-        Document mets = db.parse(getClass().getResourceAsStream("/hochschulschrift_TEST.xml"));
+        Document mets = db.parse(getClass().getResourceAsStream("/qucosa-48666.xml"));
         EpicurDissMapper mapper = new EpicurDissMapper("http://test.##AGENT##.qucosa.de/id/##PID##", "", "", true);
         Document epicurRes = mapper.transformEpicurDiss(mets);
         
         Assert.assertEquals("epicur", epicurRes.getDocumentElement().getTagName());
-    }
-
-    private XMLSerializer serializeEpicur(Document document) {
-        OutputFormat outputFormat = new OutputFormat(document);
-        outputFormat.setOmitXMLDeclaration(true);
-        StringWriter stringWriter = new StringWriter();
-        XMLSerializer serialize = new XMLSerializer(stringWriter, outputFormat);
-        return serialize;
     }
 }
